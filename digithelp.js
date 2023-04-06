42185a3f10542a8d0859a44595ca47575f414ff9
WildandRare <75457721+WildandRare@users.noreply.github.com>
2023-04-06 10:29:15 -0400

    Add files via upload

---
+++ digithelp.js
@@ -0,0 +1,8 @@
+var S_HELP = {
+    1: `I remember the phyla started with an A. So it's either "Arthropoda" or "Annelida". I also think the word was longer than "Annelida",
+    so that leaves us with "Arthropoda". And Arthropoda is also the biggest phyla of invertebrates, so the answer is probably true. Which means the first digit is probably 1.`,
+    2: `You are incredibly lucky, because unlike you, I actually listen to Mr. Roth while he's teaching. And from that I remember that earthworms are a part of the phylum Annelida. You're welcome.`,
+    3:  `I don't remember completely but I know it was one of the phylum that are long and hard to spell.`,
+    4: `Do you not know how to do basic math or do you not remember his password? I remember his password was PORIFERA, and there was 8 suspects. I'm sure you can do the math part yourself (: Are you really going to make me do all the work? <button opt>I'VE BEEN DOING ALL THE WORK THIS WHOLE TIME</button>`,
+    5: `Look on your paper I guess.`
+}
